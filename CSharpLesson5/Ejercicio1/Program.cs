1f1c15c39e9141a4112d6494e19e36722502a2e5
enguelbertg13 <enguelbertg13@gmail.com>
2022-06-15 19:31:34 -0300

    commit ejercicios

---
+++ CSharpLesson5/Ejercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿var nombre = "";
+var email = "";
+var cupon = "";
+double descuento = 0.10;
+double precio = 10.50;
+
+Console.WriteLine("Por favor ingrese su nombre...");
+nombre = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese su email...");
+email = Console.ReadLine();
+
+Console.WriteLine("Posee un cupon de descuento? Ingrese el codigo");
+cupon = Console.ReadLine();
+
+if (cupon.ToUpper() == "S")
+{
+    precio = precio - (precio * descuento);
+    Console.WriteLine("Precio producto con descuento: {0}", precio);    
+}
+else
+{
+    Console.WriteLine("Precio producto sin descuento: {0}", precio);
+}
+Console.ReadKey();
---
+++ CSharpLesson5/Ejercicio2/Program.cs
@@ -0,0 +1,34 @@
+﻿var input = "";
+
+Console.WriteLine("1. JavaScript");
+Console.WriteLine("2. C++");
+Console.WriteLine("3. JAVA");
+Console.WriteLine("4. C#");
+Console.WriteLine("5. PYTHON");
+
+Console.WriteLine();
+Console.WriteLine("Por favor ingrese una opcion...");
+input = Console.ReadLine();
+
+switch (input.ToUpper())
+{
+    case "1":
+        Console.WriteLine("LENGUAJE SELLECCIONADO: JavaScript");
+        break;
+    case "2":
+        Console.WriteLine("LENGUAJE SELLECCIONADO: C++");
+        break;
+    case "3":
+        Console.WriteLine("LENGUAJE SELLECCIONADO: JAVA");
+        break;
+    case "4":
+        Console.WriteLine("LENGUAJE SELLECCIONADO: C#");
+        break;
+    case "5":
+        Console.WriteLine("LENGUAJE SELLECCIONADO: PYTHON");
+        break;
+    default:
+        Console.WriteLine("Opcion incorrecta...");
+        break;
+}
+Console.ReadKey();      
